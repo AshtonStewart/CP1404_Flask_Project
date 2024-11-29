1ff41f5f67e29fc49a4f4587bb0fb77150fe3651
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-29 23:07:34 +1000

    Add Celcius to Farenheit converter

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+CP1404 - Practical 10
+Ashton Jack Stewart
+Flask demonstration
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +11,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<h1>> Hello World! :) <h1>'
+    return '<h1> Hello World! :) <h1>'
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+def convert_to_calcius(C_temperature):
+    """Convert a celcius temperature input to Farenheit"""
+    F_temperature = C_temperature * 1.8 + 32
+    return F_temperature
 
 if __name__ == '__main__':
     app.run()
