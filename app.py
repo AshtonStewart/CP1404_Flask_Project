ebf1005da6c1025fff899d777e0b11f1ee9e0750
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-29 23:15:03 +1000

    Add all necessary functions

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return '<h1> Hello World! :) <h1>'
 
+
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
@@ -23,6 +24,13 @@ def convert_to_calcius(C_temperature):
     """Convert a celcius temperature input to Farenheit"""
     F_temperature = C_temperature * 1.8 + 32
     return F_temperature
+
+
+@app.route('/fahrenheit')
+@app.route('/fahrenheit/<C_temperature>')
+def convert_fahrenheit(Temp=20):
+    return F"<h1>{Temp} in Celcius equals {convert_to_calcius(Temp)} in Farenheit. </h1>"
+
 
 if __name__ == '__main__':
     app.run()
